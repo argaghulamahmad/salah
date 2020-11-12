53674e273ff6d5d188525a34fb6cdeb3218f0a6f
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2020-11-12 20:26:09 +0700

    Update some components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
+import AllStories from '../components/all-stories'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
@@ -8,8 +7,6 @@ import Head from 'next/head'
 import React from "react";
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
@@ -18,17 +15,7 @@ export default function Index({ allPosts }) {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {allPosts.length > 0 && <AllStories posts={allPosts} />}
         </Container>
       </Layout>
     </>
