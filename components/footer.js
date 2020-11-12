325f02185505e91d060d48fffcd11901ce3f3e61
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2020-11-12 20:04:01 +0700

    Add link to homepage at footer.js

--- components/footer.js
+++ components/footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
         <footer className="bg-accent-1 border-t border-accent-2">
             <Container>
                 <div className="py-28 flex flex-col lg:flex-row items-center">
-                    <h3 className="text-xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-                        Developed by argaghulamahmad.dev
+                    <h3 className="text-xl lg:text-2xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+                        Developed by <a href="https://argaghulamahmad.dev" className="hover:underline">argaghulamahmad.dev</a>
                     </h3>
                     <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
                         <a
